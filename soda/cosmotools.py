leapfrog update and fixed typos in r200 computation

soda/cosmotools.py
@@ -46,11 +46,12 @@ def rvir2(Mvir, z):
 
 
 def r200(M200):
+    z = 0
     M200 = M200 * units.Msun
     Delta200 = 200
     pcrit = rho_crit(z)
-    R200 = (3*Mvir / (4 * np.pi * Deltavir * pcrit * Omega0))**(1/3.)
-    R200 = Rvir.to(units.kpc)
+    R200 = (3*M200 / (4 * np.pi * Delta200 * pcrit * Omega0))**(1/3.)
+    R200 = R200.to(units.kpc)
     return R200
 
 def concentration(Mvir):